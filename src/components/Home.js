6ff88cd0d8febc790d4e37bf65a2863f258838ab
Austin Morales <harrenaustint@gmail.com>
2020-11-10 13:52:08 -0500

    pushed to github

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 class Home extends Component {
     state = {
         posts: []
@@ -18,10 +20,14 @@ class Home extends Component {
             posts.map(post => {
             return (
                    <div className='post card' key={post.id}>
+                     <img src={Pokeball} alt="Pokeball"/>
                      <div className="card-content">
-                     <span className="card-title">
+                     <Link to={'/' + post.id}>
+                     <span className="card-title red-text">
                         { post.title }
                      </span>
+                     </Link>
+                     
                      <p>{post.body}</p>
                      </div>  
                    </div>
@@ -34,7 +40,7 @@ class Home extends Component {
         )
     return (
 
-        <div className='container'>
+        <div className='container home'>
             <h4 className="center">Home</h4>
             <p>{postList}</p>
         </div>
